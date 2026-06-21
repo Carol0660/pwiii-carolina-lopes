019425d99f03f303c899a9e1f472760cffd48829
Carolina Lopes <carolcandela006@gmail.com>
2026-06-21 12:01:57 -0300

    Atividade controller

--- biblioteca/routes/web.php
+++ biblioteca/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use app\Http\Controllers\LivroController;
+use App\Http\Controllers\GameController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Quando o usuário acessar seu-site.com/livros,
+//o método 'index' do controller entra em ação!
+Route::get('/livros', [LivroController::class, 'index']);
+Route::get('/buscar/{nome}', [GameController::class, 'search']);
